5aba094205c0ecdb87e16236d27acb2fd3f4c108
bangkittrih <bangkittriharsongko@gmail.com>
2018-06-05 09:24:14 +0700

    Android Studio (date)
    Belum selesai 100 % dikarenakan masih bingung untuk menampilkan maps pada apk

---
+++ Bangkit2/app/src/main/java/com/example/bangkit/bangkit/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.example.bangkit.bangkit
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
+import kotlin.collections.ArrayList
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val c: Calendar = Calendar.getInstance()
+        val year: Int = c.get(Calendar.YEAR)
+        val month: Int = c.get(Calendar.MONTH)
+        val day: Int = c.get(Calendar.DAY_OF_MONTH)
+
+        //**Menampilkan Tanggal (dd/mm/yyyy/)**//
+        date.text = "$day/$month/$year"
+    }
+
+}
+
